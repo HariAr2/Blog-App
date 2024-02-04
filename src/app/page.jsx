6059d88117cca3058ad518962b5e8fc84a1232ee
HariAr2 <ha7014@srmist.edu.in>
2024-02-04 21:45:20 +0530

    Styles and Layouts

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,8 @@
 import styles from "./homepage.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <div>
+    <Link href="/">Hello</Link>
+  </div>;
 }
