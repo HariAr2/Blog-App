a6ebdb919db0d973a9e682a806206ba29f126a2d
Sayeeram05 <sr6172@srmist.edu.in>
2024-02-10 19:27:19 +0530

    Initial commit

---
+++ src/components/categoryList/CategoryList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./categoryList.module.css"
+
+
+function CategoryList(){
+    return(
+        <div className={styles.container}>CategoryList</div>
+    );
+}
+
+export default CategoryList
