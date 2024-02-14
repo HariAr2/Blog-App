28297d9e176e462c6e941b7501adff6c199a8736
HariAr2 <ha7014@srmist.edu.in>
2024-02-14 21:34:57 +0530

    Menu.jsx

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "./menu.module.css";
 
 function Menu() {
-  return <div className={styles.container}>Menu</div>;
+  return <div className={styles.container}>
+    <h2>{"What's Hot"}</h2>
+    <h1>Most Popular</h1>
+    <div className={styles.item}></div>
+  </div>;
 }
 
 export default Menu;
