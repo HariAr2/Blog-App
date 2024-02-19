c54d966ad4287f90719089edb822e76db7f83d93
HariAr2 <ha7014@srmist.edu.in>
2024-02-20 01:21:44 +0530

    issue in page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import Featured from "@/components/featured/Featured";
 import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/Menu/Menu";
-
+//need to check page.jsx and layout.js
 export default function Home() {
   return (
     <div className={styles.container}>
