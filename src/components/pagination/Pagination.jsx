efdf6119c10429d6452d466eea5bf4b02aac7db0
HariAr2 <ha7014@srmist.edu.in>
2024-02-24 18:16:53 +0530

    Completed Pagination

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -4,7 +4,10 @@ import styles from "./pagination.module.css"
 
 function Pagination(){
     return(
-        <div className={styles.container}>Pagination</div>
+        <div className={styles.container}>
+            <button className={styles.button} >Previous</button>
+            <button className={styles.button} >Next</button>
+        </div>
     );
 }
 
