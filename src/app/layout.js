6ba92003218e27e9db1ba89ea70155556a72b0bd
HariAr2 <ha7014@srmist.edu.in>
2024-02-25 21:28:30 +0530

    Completed Login Page

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import Footer from '@/components/footer/Footer'
 import { ThemeContextProvider } from '@/context/ThemeContext'
 import ThemeProvider from '@/providers/ThemeProvider'
 
-//total time spent: 13 hours
+//total time spent: 15 hours
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
