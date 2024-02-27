226ea0fb5b915e79b261326483b748c1e6976bdf
HariAr2 <ha7014@srmist.edu.in>
2024-02-27 20:40:50 +0530

    error 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,17 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/navbar/Navbar'
-import Footer from '@/components/footer/Footer'
-import { ThemeContextProvider } from '@/context/ThemeContext'
-import ThemeProvider from '@/providers/ThemeProvider'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
+import { ThemeContextProvider } from "@/context/ThemeContext";
+import ThemeProvider from "@/providers/ThemeProvider";
 
 //total time spent: 15 hours
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
-}
+  title: "Blog App",
+  description: "The best blog app!",
+};
 // layout of the Blog-App
 export default function RootLayout({ children }) {
   return (
@@ -30,5 +30,5 @@ export default function RootLayout({ children }) {
         </ThemeContextProvider>
       </body>
     </html>
-  )
+  );
 }
