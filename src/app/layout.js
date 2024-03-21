60a4a4b05ce2fba0bbec113bc8b46c60ba9a6519
HariAr2 <ha7014@srmist.edu.in>
2024-03-21 22:16:18 +0530

    formatted the files

--- src/app/layout.js
+++ src/app/layout.js
@@ -35,3 +35,5 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+// comments
+// need documentation for better understanding
--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -4,7 +4,7 @@ import styles from "./writePage.module.css";
 import Image from "next/image";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.bubble.css";
-// need change @line 27 marked 
+
 const WritePage = () => {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
@@ -42,6 +42,4 @@ const WritePage = () => {
 };
 
 export default WritePage;
-// error in Line 24 and 26 (Image not found)....
-//timestamp: 2:34:48
-// try to debug this error
+
